f87cc9d386cf854ca6d8ff00d3fafdc451a1b8bd
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2022-12-09 05:49:29 +0600

    odd even done!

--- odd-even.py
+++ odd-even.py
@@ -1,1 +1,9 @@
 # Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
+
+number = input ("Enter an integer number: ")
+
+if int(number) % 2 == 0 :
+    print ("The input number is an Even number:", number)
+
+else:
+    print ("The input number is an Odd number:", number)
