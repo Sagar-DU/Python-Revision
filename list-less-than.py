5d7d38d2de45db81f7a7adde5ebc346f5320b630
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2022-12-09 13:45:12 +0600

    code done

--- list-less-than.py
+++ list-less-than.py
@@ -1,3 +1,17 @@
 # Take a list, say for example this one:
 #   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # and write a program that prints out all the elements of the list that are less than 5.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+less5 = []
+
+# Multi line
+# for x in a:
+#     if x < 5:
+#         less5.append(x)
+
+# In one line 
+less5 = [x for x in a if x < 5]
+
+
+print (less5)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+#         less5 = less5.extend("x")
+
+# print (less5)
